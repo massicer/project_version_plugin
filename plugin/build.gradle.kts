f9f46f9a12e73f8d20e47f098703861256cd3fc2
Massimiliano Ceriani <massimiliano.ceriani@casavo.com>
2022-01-06 18:04:09 +0100

    feat: store the project version in properties file

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,8 +1,12 @@
 group = "com.flamingosw"
+version = "0.1.0"
+
 
 plugins {
     `java-gradle-plugin`
     kotlin("jvm") version "1.6.10"
+    id("maven-publish")
+    id("com.gradle.plugin-publish") version "0.19.0"
 }
 
 repositories {
@@ -19,12 +23,20 @@ tasks.test {
     useJUnitPlatform()
 }
 
+pluginBundle {
+    website = "https://github.com/massicer/project_version_plugin"
+    vcsUrl = "https://github.com/massicer/project_version_plugin"
+    tags = listOf("version", "properties")
+}
+
 
 gradlePlugin {
     plugins {
         create("project_version_plugin") {
             id = "com.flamingosw.project_version_plugin"
             implementationClass = "com.flamingosw.project_version_plugin.VersionPlugin"
+            displayName = "Extract version plugin"
+            description = "Extract your gradle project version writing it in the application properties file."
         }
     }
 }
